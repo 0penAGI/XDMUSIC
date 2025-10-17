d9d896648f49b67853c131578107ab0f268fc73b
0penAGI <thedubsty@gmail.com>
2025-10-17 10:51:53 +0700

    Update __init__.py

--- xdust/__init__.py
+++ xdust/__init__.py
@@ -1,1 +1,4 @@
+from .xdust import BinaryQubit, quantum_punk_avantgarde, TrackExpansionEngine, create_full_track_from_seed
 
+__version__ = "0.1.0"
+__all__ = ["BinaryQubit", "quantum_punk_avantgarde", "TrackExpansionEngine", "create_full_track_from_seed"]
